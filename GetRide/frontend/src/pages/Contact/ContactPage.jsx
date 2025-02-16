b2a1863413257ca234588198a510ca3a55827682
ketibm <k.matenicharska@outlook.com>
2025-02-16 22:21:18 +0100

    Third

--- GetRide/frontend/src/pages/Contact/ContactPage.jsx
+++ GetRide/frontend/src/pages/Contact/ContactPage.jsx
@@ -4,7 +4,50 @@ import "./ContactPage.css";
 
 const ContactPage = () => {
   const { t } = useTranslation();
-  return <div></div>;
+  return (
+    <div className="contact-container">
+      <section className="contact-container-form">
+        <form onSubmit={handleSubmit}>
+          <h4>{t("question")}</h4>
+          <div className="contact-container-form-input">
+            <input
+              type="email"
+              id="email"
+              name="email"
+              value={formData.email}
+              onChange={handleInputChange}
+              placeholder={t("emailPlaceholder")}
+              autoComplete="email"
+              required
+            />
+            <textarea
+              id="message"
+              name="message"
+              value={formData.message}
+              onChange={handleInputChange}
+              placeholder={t("messagePlaceholder")}
+              required
+            ></textarea>
+          </div>
+          <div className="contact-container-button">
+            <MyButton
+              type="submit"
+              name={t("sendMessage")}
+              onClick={handleSubmit}
+              disabled={isSubmitting}
+            />
+          </div>
+        </form>
+      </section>
+      {showModal && (
+        <ModalCard
+          show={showModal}
+          message={modalMessage}
+          closeModal={closeModal}
+        />
+      )}
+    </div>
+  );
 };
 
 export default ContactPage;
