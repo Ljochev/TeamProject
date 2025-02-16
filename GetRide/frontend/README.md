7dfb5c965e4fdf0ad5cd650feeabe732a2902de9
Ljochev <ljochev@gmail.com>
2025-02-16 01:23:06 +0100

    Starting Login pages

--- GetRide/frontend/README.md
+++ GetRide/frontend/README.md
@@ -6,3 +6,15 @@ Currently, two official plugins are available:
 
 - [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
 - [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
+
+background
+light orange - #EF965D
+darker orange = #E88553
+Dark orange - #C85E39
+grey - #BFB09E
+light grey - #D0BEAD
+
+black - #1A2B33
+
+
+Button orange #FE8455
--- GetRide/frontend/src/components/LogPageComps/LogPage.jsx
+++ GetRide/frontend/src/components/LogPageComps/LogPage.jsx
@@ -1,9 +1,33 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
+import './LogPage.css';
 
-const LogPage = () => {
-  return (
-    <div>LogPage</div>
-  )
-}
+const LogPage = ({logData}) => {
+    return (
+      <div className="log_page_wrap">
+      <div className="log_page_info">
+        <div>
+        <h1>ЗАПОЧНИ ГО ТВОЕТО ПАТУВАЊЕ!</h1>
+        <span>Секое споделено возење е чекор кон почиста планета и подобар свет за идните генерации!</span>
+        </div>
+        <div className="log_page_logo">
+        <Link to='/'>
+        Home Page
+        </Link>
+          <p>ourDomain.com</p>
+        </div>
+      </div>
+      <div className="log_section">
+      {/* <img className="log_rocket" src={rocket}/> */}
+      {/* <Link to='/'><img className="mentor_logo_img" src={MentorLogo}/></Link> */}
+      <Link to='/'>Home Page</Link>
 
-export default LogPage
+      <div className="log_section_component">
+      {logData}
+      </div>
+      </div>
+    </div>
+    )
+  }
+  
+  export default LogPage
